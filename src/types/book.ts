341c90b85607a86b123de5ef5abfaf94ff027852
ghirahm <ghirahmadanis@gmail.com>
2025-05-09 23:19:18 +0700

    feat: connect API

--- src/types/book.ts
+++ src/types/book.ts
@@ -1,3 +1,8 @@
+interface Category {
+    id: number;
+    name: string;
+}
+
 export interface Book {
     id: number;
     title: string;
@@ -7,4 +12,6 @@ export interface Book {
     price: number;
     discount_percent: number;
     description: string;
+    stock: number;
+    categories?: Category[];
 }
